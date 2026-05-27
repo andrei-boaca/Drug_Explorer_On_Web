081a84f2e8eb693acc25d9c947d92133bc9bb82b
andrei-boaca <boacaandrei22@gmail.com>
2026-05-27 17:41:32 +0300

    Fix: drop and recreate DB user to avoid stale password

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -45,7 +45,8 @@ apt-get install -yq nginx mysql-server \
 echo "[3/7] Configurare MySQL..."
 mysql -u root <<SQL
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
+DROP USER IF EXISTS '${DB_USER}'@'localhost';
+CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'localhost';
 FLUSH PRIVILEGES;
 SQL
